71095d4ddc3f044ee2aa7c74bc900bdd52ce8524
juchan <73540042+mtaa5223@users.noreply.github.com>
2025-06-14 01:03:36 +0900

    [level 0] Title: 할 일 목록, Time: 0.30 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public string[] solution(string[] todo_list, bool[] finished) {
+        
+        List<string> answer = new List<string>();
+
+        for (int i = 0; i < todo_list.Length; i++)
+        {
+            if (finished[i] == false)
+            {
+                answer.Add(todo_list[i]);
+            }
+        }
+        
+        return answer.ToArray();
+    }
+}
