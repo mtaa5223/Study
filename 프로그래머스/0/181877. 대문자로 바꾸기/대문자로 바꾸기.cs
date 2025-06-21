4d041f795939989eae9d68342cf3277a426e4523
juchan <73540042+mtaa5223@users.noreply.github.com>
2025-06-21 16:17:48 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.51 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        string answer = "";
+        foreach (var VARIABLE in myString)
+        {
+            answer = myString.ToUpper();
+        }
+        return answer;
+    }
+}
