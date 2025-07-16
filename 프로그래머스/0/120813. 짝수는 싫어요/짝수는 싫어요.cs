be5b80a6448a9ad4d0fa595e8490233e59584444
juchan <73540042+mtaa5223@users.noreply.github.com>
2025-07-17 00:01:12 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.52 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public int[] solution(int n) {
+        List<int> answer = new List<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 != 0)
+            {
+                answer.Add(i);
+            }    
+        }
+        return answer.ToArray();
+    }
+}
